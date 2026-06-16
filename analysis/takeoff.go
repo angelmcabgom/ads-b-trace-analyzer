a4bc3b64db70b4cb27ad48f48b6a32414197e3a0
angel <angelmcabgom@gmail.com>
2026-06-16 22:58:33 +0200

    WIP: analysis takeoff, no idea if it works, will finish tmrw

---
+++ analysis/takeoff.go
@@ -0,0 +1,38 @@
+package analysis
+
+import (
+	"ads-b-geom-parser/trace"
+	"fmt"
+)
+
+func DetectTakeOff(points []trace.TracePoint) bool {
+    if len(points) < 5 {
+        return false
+    }
+
+    // average first 5 points to smooth out baro noise
+	// things do get pretty rocky out there
+    var altSum int
+    for _, p := range points[:5] {
+        altSum += p.Altitude
+    }
+    groundAlt := altSum / 5
+
+    consecutiveClimbs := 0
+    for _, point := range points {
+        climbing := point.Altitude > groundAlt+50 &&
+            point.VerticalRate > 800 &&
+            point.GroundSpeed > 50
+
+        if climbing {
+            consecutiveClimbs++
+            if consecutiveClimbs >= 3 {
+				fmt.Printf("takeoff detected omg!!")
+                return true
+            }
+        } else {
+            consecutiveClimbs = 0
+        }
+    }
+    return false
+}
