1279b3f56127def8313b6049f1e5c5aeb8c8dcea
angel <angelmcabgom@gmail.com>
2026-06-18 18:41:58 +0200

    WIP: some more progress in the landing analysis

--- analysis/landing.go
+++ analysis/landing.go
@@ -5,5 +5,22 @@ import (
 )
 
 func DetectLanding(points []trace.TracePoint) bool {
+	
+	// return false since i cant determine if theres a landing with so few points
+	if len(points) < 10 {
+		return false
+	}
+
+	// this approach assumes theres a landing present in the trace
+	// aka its a full trace, some traces might not have this tho
+	// get airport altitute
+	var altSum int
+	for _, p := range points[len(points) - 10:]{
+		altSum += p.Altitude
+	}
+	groundAlt := altSum / 10
+
+	
+	
 	return false
 }
