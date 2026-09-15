aba9c2aa33f34bd7ecf50a8b891a03e39d13b9ed
angelmcabgom <angelmcabgom@gmail.com>
2026-09-15 20:48:44 +0200

    feat: dataset parsed and ready to be uploaded into db

--- airports/insert_airports.go
+++ airports/insert_airports.go
@@ -7,26 +7,43 @@ import (
 	"io"
 	"log"
 	"net/http"
+	"strconv"
 	"time"
-)
 
+	_ "github.com/lib/pq"
+)
 
 // need type for row
 
 type AirportRow struct {
-	id 	string
+	id 				int32
+	ident 			string
+	airportType		string
+	name			string
+	location		CoordsPoint
+	elevation		int32
+	iso_country		string
+	municipality	string
+	icao_code		string
+	iata_code		string
 }
+
+type CoordsPoint struct {
+	Latitude       float32
+	Longitude      float32
+}
+
 
 func main(){
 	start := time.Now()
 	rows := downloadAirportData()
 	elapsed := time.Since(start)
-	fmt.Print(rows)
+	fmt.Print(len(rows))
 	fmt.Printf("Total time: %v\n", elapsed)
 }
 
 // can easily be refactored into a general download function
-func downloadAirportData() [][]string {
+func downloadAirportData() []AirportRow {
 	resp, err := http.Get("https://davidmegginson.github.io/ourairports-data/airports.csv")
 	
 	if err != nil {
@@ -41,10 +58,11 @@ func downloadAirportData() [][]string {
 		log.Fatal("Error reading downloaded data")
 	}
 
-
 	reader := csv.NewReader(bytes.NewBuffer(body))
 	
-	rows := [][]string{}
+	airportRows := []AirportRow{}
+
+	// skip first row
 	for {
 		row, err := reader.Read()
 
@@ -55,8 +73,48 @@ func downloadAirportData() [][]string {
 			}
 		}
 
-		rows = append(rows, row)
+
+		airportRows = append(airportRows, AirportRow{
+			id: parseInt32(row[0]),
+			ident: row[1],
+			airportType: row[2],
+			name: row[3],
+			location: CoordsPoint{
+				Latitude: parseFloat32(row[4]),
+				Longitude: parseFloat32(row[5]),
+			},
+			elevation: parseInt32(row[6]),
+			iso_country: row[8],
+			municipality: row[10],
+			icao_code: row[12],
+			iata_code: row[13],
+		})
 	}
 
-	return rows
+	return airportRows
+}
+
+func parseInt32(s string) int32 {
+	v, err := strconv.ParseInt(s, 10, 32)
+
+	if err != nil {
+		fmt.Printf("Failed to parse int 32, %e: %v", err, v)
+	}
+
+	return int32(v)
+}
+
+func parseFloat32(s string) float32 {
+	v, err := strconv.ParseFloat(s, 32)
+
+	if err != nil {
+		fmt.Printf("Failed to parse float 32, %e: %v", err, v)
+	}
+	return float32(v)
+}
+
+func insertIntoDb(csvPath string) bool {
+	
+	
+	return true
 }
